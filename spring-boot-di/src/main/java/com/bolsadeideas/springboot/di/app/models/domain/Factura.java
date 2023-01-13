2afc2f9c94f535df36c4f09699220e88474b7966
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-01-13 18:42:39 -0500

    Ciclo de vida del componente

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -2,6 +2,9 @@ package com.bolsadeideas.springboot.di.app.models.domain;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -18,6 +21,17 @@ public class Factura {
 	@Autowired
 	//@Qualifier("itemsFacturaOficina")
 	private List<ItemFactura> items;
+	
+	@PostConstruct
+	public void inicializar() {
+		cliente.setNombre(cliente.getNombre().concat(" ").concat("Jarlin"));
+		descripcion = descripcion.concat(" del cliente: ").concat(cliente.getNombre());
+	}
+	
+	@PreDestroy
+	public void destruir() {
+		System.out.println("Factura destruida: ".concat(descripcion));
+	}
 
 	
 	public String getDescripcion() {
