98ecb28e2c844ab5d69c1967d7a07c9931b45496
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-01-13 10:49:24 -0500

    Inyectando mediante la interface

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -3,9 +3,10 @@ package com.bolsadeideas.springboot.di.app.models.service;
 import org.springframework.stereotype.Service;
 
 //@Component es lo mismo que Service la diferencia es semantica
-@Service
-public class MiServicio {
+@Service("miServicioSimple")
+public class MiServicio implements IServicio {
 	
+	@Override
 	public String operacion() {
 		return "ejecutando algún proceso importante...";
 	}
