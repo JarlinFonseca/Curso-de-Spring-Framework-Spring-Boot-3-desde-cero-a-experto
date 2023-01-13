8a5bebbcd49ffbb7b75300cc2a36516f79f6d47b
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-01-13 10:07:42 -0500

    Implementando inyección de dependencia con la anotación @Autowired

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -1,5 +1,9 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
+import org.springframework.stereotype.Service;
+
+//@Component es lo mismo que Service la diferencia es semantica
+@Service
 public class MiServicio {
 	
 	public String operacion() {
