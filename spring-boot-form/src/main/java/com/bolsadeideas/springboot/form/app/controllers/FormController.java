c33196984e55d89a637fe8926ab50785693650dc
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-01-16 17:42:54 -0500

    Mapeando los campos del formulario al objeto model

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 @Controller
@@ -17,15 +15,25 @@ public class FormController {
 		return "form";
 	}
 	
+	/*
+	 * @PostMapping("/form") public String procesar(Model model, @RequestParam(name
+	 * = "username") String username,
+	 * 
+	 * @RequestParam String password,
+	 * 
+	 * @RequestParam String email) {
+	 * 
+	 * Usuario usuario = new Usuario(); usuario.setUsername(username);
+	 * usuario.setEmail(email); usuario.setPassword(password);
+	 * 
+	 * model.addAttribute("titulo", "Resultado form"); model.addAttribute("usuario",
+	 * usuario);
+	 * 
+	 * return "resultado"; }
+	 */
+	
 	@PostMapping("/form")
-	public String procesar(Model model, @RequestParam(name = "username") String username,
-			@RequestParam String password, 
-			@RequestParam String email) {
-		
-		Usuario usuario = new Usuario();
-		usuario.setUsername(username);
-		usuario.setEmail(email);
-		usuario.setPassword(password);
+	public String procesar(Usuario usuario, Model model) {
 		
 		model.addAttribute("titulo", "Resultado form");
 		model.addAttribute("usuario", usuario);
@@ -33,5 +41,4 @@ public class FormController {
 		return "resultado";
 	}
 	
-
 }
