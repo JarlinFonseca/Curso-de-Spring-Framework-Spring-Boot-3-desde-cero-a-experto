5576c1b73a9432e25e086f35e9da6b56f5312dd3
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-01-19 11:17:04 -0500

    Añadiendo los mensajes de errores en la vista del formulario

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
@@ -17,7 +18,9 @@ public class FormController {
 	
 	@GetMapping("/form")
 	public String form(Model model) {
+		Usuario usuario = new Usuario();
 		model.addAttribute("titulo", "Formulario Usuarios");
+		model.addAttribute("user", usuario);
 		return "form";
 	}
 	
@@ -39,7 +42,7 @@ public class FormController {
 	 */
 	
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model) {
 		
 		model.addAttribute("titulo", "Resultado form");
 		
@@ -49,8 +52,8 @@ public class FormController {
 				errores.put(err.getField(), "El campo ".concat(err.getField().concat(" ").concat(err.getDefaultMessage())));
 			});
 			
-			model.addAttribute("errores", errores);
-			return "";
+			model.addAttribute("error", errores);
+			return "form";
 		}
 		
 		model.addAttribute("usuario", usuario);
