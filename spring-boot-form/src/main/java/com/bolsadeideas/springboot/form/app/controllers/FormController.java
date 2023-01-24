008b5cba8b78f3c825e2344cbd4455e0a4bd2824
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-01-24 11:16:01 -0500

    Checkbox booleano true o false

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -121,6 +121,7 @@ public class FormController {
 		usuario.setNombre("John");
 		usuario.setApellido("Doe");
 		usuario.setIdentificador("123.456.789-K");
+		usuario.setHabilitar(true);
 		
 		model.addAttribute("titulo", "Formulario Usuarios");
 		model.addAttribute("usuario", usuario);
--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -21,106 +21,134 @@ import com.bolsadeideas.springboot.form.app.validation.IdentificadorRegex;
 import com.bolsadeideas.springboot.form.app.validation.Requerido;
 
 public class Usuario {
-	
-	//@Pattern(regexp = "[0-9]{2}[.,][\\d]{3}[.,][\\d]{3}[-][A-Z]{1}")
+
+	// @Pattern(regexp = "[0-9]{2}[.,][\\d]{3}[.,][\\d]{3}[-][A-Z]{1}")
 	@IdentificadorRegex
 	private String identificador;
-	
-	//@NotEmpty(message = "El nombre no puede ser vacio")
+
+	// @NotEmpty(message = "El nombre no puede ser vacio")
 	private String nombre;
-	
-	//@NotEmpty
+
+	// @NotEmpty
 	@Requerido
 	private String apellido;
-	
+
 	@NotBlank
-	@Size(min = 3, max =8)
+	@Size(min = 3, max = 8)
 	private String username;
-	
+
 	@NotEmpty
 	private String password;
-	
+
 	@Requerido
 	@Email(message = "Correo con formato incorrecto")
 	private String email;
-	
+
 	@NotNull
 	@Min(5)
 	@Max(5000)
 	private Integer cuenta;
-	
+
 	@NotNull
 	@Past
-	//@DateTimeFormat(pattern = "yyyy-MM-dd")
+	// @DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date fechaNacimiento;
-	
-	//@Valid
+
+	// @Valid
 	@NotNull
 	private Pais pais;
-	
+
 	@NotEmpty
 	private List<Role> roles;
-	
-	
+
+	private Boolean habilitar;
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public String getIdentificador() {
 		return identificador;
 	}
+
 	public void setIdentificador(String identificador) {
 		this.identificador = identificador;
 	}
+
 	public Integer getCuenta() {
 		return cuenta;
 	}
+
 	public void setCuenta(Integer cuenta) {
 		this.cuenta = cuenta;
 	}
+
 	public Date getFechaNacimiento() {
 		return fechaNacimiento;
 	}
+
 	public void setFechaNacimiento(Date fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}
+
 	public Pais getPais() {
 		return pais;
 	}
+
 	public void setPais(Pais pais) {
 		this.pais = pais;
 	}
+
 	public List<Role> getRoles() {
 		return roles;
 	}
+
 	public void setRoles(List<Role> roles) {
 		this.roles = roles;
 	}
-	
+
+	public Boolean getHabilitar() {
+		return habilitar;
+	}
+
+	public void setHabilitar(Boolean habilitar) {
+		this.habilitar = habilitar;
+	}
+
 }
