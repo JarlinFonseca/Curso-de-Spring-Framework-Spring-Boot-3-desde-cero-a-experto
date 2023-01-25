02bedf33c53dd2336d5327dca460124493fdd631
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-01-25 17:50:32 -0500

    . Manejando excepción personalizada creando clases Model y Service

---
+++ spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/services/UsuarioServiceImp.java
@@ -0,0 +1,44 @@
+package com.bolsadeideas.springboot.error.app.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bolsadeideas.springboot.error.app.models.domain.Usuario;
+
+@Service
+public class UsuarioServiceImp implements UsuarioService {
+	
+	private List<Usuario> lista;
+
+	
+	public UsuarioServiceImp() {
+		this.lista = new ArrayList<>();
+		this.lista.add(new Usuario(1, "Jarlin", "Fonseca"));
+		this.lista.add(new Usuario(2, "Pepa", "García"));
+		this.lista.add(new Usuario(3, "Lalo", "Mena"));
+		this.lista.add(new Usuario(4, "Luci", "Fernández"));
+		this.lista.add(new Usuario(5, "Pato", "González"));
+		this.lista.add(new Usuario(6, "Paco", "Rodríguez"));
+		this.lista.add(new Usuario(7, "Juan", "Gómez"));
+	}
+
+	@Override
+	public List<Usuario> listar() {
+		return this.lista;
+	}
+
+	@Override
+	public Usuario obtenerPorId(Integer id) {
+		Usuario resultado = null;
+		for (Usuario usuario : lista) {
+			if(usuario.getId().equals(id)) {
+				resultado = usuario;
+				break;
+			}
+		}
+		return resultado;
+	}
+
+}
