fcc47fe7ba8de0623d648ee3ee4050ad14433649
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-01-25 11:45:45 -0500

    Saliendo del interceptor en peticiones POST http - return en post handle

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -44,7 +44,9 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 		
-		if(request.getMethod().equalsIgnoreCase("post")) return;
+		if(request.getMethod().equalsIgnoreCase("post")) { 
+			return;
+			}
 		
 		long tiempoFin = System.currentTimeMillis();
 		long tiempoInicio = (Long)request.getAttribute("tiempoInicio");
