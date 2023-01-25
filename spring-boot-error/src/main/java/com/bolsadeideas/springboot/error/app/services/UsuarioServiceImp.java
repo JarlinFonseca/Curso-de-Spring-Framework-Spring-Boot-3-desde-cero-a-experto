09402acb8195b885fab2bd518e2c4084554f9e0b
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-01-25 18:30:51 -0500

    Lanzamiento de exepción usando Api Optional de Java 8

--- spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/services/UsuarioServiceImp.java
+++ spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/services/UsuarioServiceImp.java
@@ -2,6 +2,7 @@ package com.bolsadeideas.springboot.error.app.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -39,6 +40,12 @@ public class UsuarioServiceImp implements UsuarioService {
 			}
 		}
 		return resultado;
+	}
+
+	@Override
+	public Optional<Usuario> obtenerPorIdOptional(Integer id) {
+		Usuario usuario = this.obtenerPorId(id);
+		return Optional.ofNullable(usuario);
 	}
 
 }
