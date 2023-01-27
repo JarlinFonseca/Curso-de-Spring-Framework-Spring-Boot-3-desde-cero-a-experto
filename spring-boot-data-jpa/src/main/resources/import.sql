29ffca4a60991234a585296152dfab838395b1ec
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-01-27 16:19:02 -0500

    Creando la vista listar.html

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/*Populate tables*/
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Jarlin', 'Fonseca', 'jarlinfonseca@gmail.com', '2023-01-27');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2023-01-27');
