0ac6edb92d94f575ff104976c671ef02d71705e2
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-01-29 17:32:42 -0500

    Agregando la clase Service

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImp.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImp.java
@@ -6,8 +6,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
 @Repository("clienteDaoImp")
@@ -17,13 +15,12 @@ public class ClienteDaoImp implements IClienteDao{
 	private EntityManager em;
 
 	@SuppressWarnings("unchecked")
-	@Transactional(readOnly=true )
 	@Override
 	public List<Cliente> findAll() {
 		return em.createQuery("FROM Cliente").getResultList();
 	}
 
-	@Transactional
+	
 	@Override
 	public void save(Cliente cliente) {
 		if(cliente.getId() !=null && cliente.getId() > 0) {
@@ -34,13 +31,11 @@ public class ClienteDaoImp implements IClienteDao{
 	}
 
 	@Override
-	@Transactional(readOnly=true )
 	public Cliente findOne(Long id) {
 		return em.find(Cliente.class, id);
 	}
 
 	@Override
-	@Transactional
 	public void delete(Long id) {
 		em.remove(findOne(id));	
 	}
